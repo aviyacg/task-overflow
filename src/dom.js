fc9de613d629fa0a85056808c20b8479b1a88a25
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-11-10 19:41:20 +0200

    Cretae dom.js with addTask loadPage loadTodoList

---
+++ src/dom.js
@@ -0,0 +1,67 @@
+export function addTask(task) {
+  const TASK = document.createElement('div');
+  TASK.classList.add('task');
+  TASK.dataset.taskId = task.id;
+
+  // checkbox
+  const CHECKBOX = document.createElement('div');
+  CHECKBOX.classList.add('checkbox');
+  TASK.appendChild(CHECKBOX);
+  // details
+  const DETAILS = document.createElement('div');
+  DETAILS.textContent = task.details;
+  TASK.appendChild(DETAILS);
+  // dueDate
+  const DATE = document.createElement('div');
+  DATE.classList.add('date');
+  DATE.textContent = task.dueDate;
+  TASK.appendChild(DATE);
+  // edit button
+  const EDIT = document.createElement('button');
+  EDIT.classList.add('edit');
+  TASK.appendChild(EDIT);
+  // delete button
+  const DELETE = document.createElement('button');
+  DELETE.classList.add('delete');
+  TASK.appendChild(DELETE);
+
+  document.querySelector('.main').appendChild(TASK);
+}
+
+export function loadTodoList(todoList) {
+  const MAIN = document.querySelector('.main');
+  // add title div
+  const TITLE = document.createElement('div');
+  TITLE.classList.add('title');
+  TITLE.textContent = todoList.title;
+  TITLE.dataset.todoListId = todoList.id;
+  MAIN.appendChild(TITLE);
+  // add all tasks divs
+  todoList.taskList.forEach((task) => { addTask(task); });
+  // add task button
+  const ADD_TASK_BUTTON = document.createElement('button');
+  ADD_TASK_BUTTON.classList.add('add-task-button');
+  ADD_TASK_BUTTON.textContent = 'Add Task';
+  MAIN.appendChild(ADD_TASK_BUTTON);
+}
+
+export function loadPage() {
+  // layout divs
+  const HEADER = document.createElement('div');
+  HEADER.classList.add('header');
+  HEADER.textContent = 'Task Overflow';
+  document.body.appendChild(HEADER);
+
+  const NAV = document.createElement('div');
+  NAV.classList.add('nav');
+  document.body.appendChild(NAV);
+
+  const MAIN = document.createElement('div');
+  MAIN.classList.add('main');
+  document.body.appendChild(MAIN);
+
+  // nav content
+  // load todoLists from storage
+  // add todolists buttons to nav
+  // add new todolist button
+}
